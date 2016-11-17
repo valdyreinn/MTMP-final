360945075b70c8619d91ae748249690d9c781004
Valdyreinn <valdyreinn@gmail.com>
2016-11-17 11:43:53 +0100

    complete animation for running and attacking

---
+++ Assets/PlayerAnimationController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimationController : MonoBehaviour {
+
+	public PlayerController player;
+
+	public void endAttacking() {
+		player.endAttacking ();
+	}
+}
--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -10,53 +10,111 @@ public class PlayerController : MonoBehaviour {
     [Tooltip("Character animator")]
     public Animator animator;
 
-	private Vector3 position;
+	private Vector3 movementPosition;
+	private Vector3 attackPosition;
     private float distance;
+	private State state;
 
 	private readonly float RAY_DISTANCE = 1000;
 
 	void Start () {
-		position = transform.position;
+		movementPosition = transform.position;
+		state = State.IDLE;
 	}
 	
 	void Update () {
-		if(Input.GetMouseButton (0)) {
-			locatePosition ();
-		} else if (Input.GetMouseButton (1)) {
-						
+		if(Input.GetMouseButtonDown (0) && isNotAttacking ()) {
+			Debug.Log ("LeftMouseButton - start");
+			locatePosition (out movementPosition);
+			stateRunning ();
+			Debug.Log ("LeftMouseButton - end");
+		} else if (Input.GetMouseButtonDown (1) && isNotAttacking ()) {
+			Debug.Log ("RightMouseButton - start");
+			locatePosition (out attackPosition);
+			stateAttacking ();
+			Debug.Log ("RightMouseButton - end");
 		}
-		moveToPosition ();
+
+		if (isRunning())
+			moveToPosition ();
 	}
 
-	void locatePosition() {
+	bool isRunning() {
+		return state == State.RUNNING;
+	}
+
+	bool isNotRunning() {
+		return !isRunning ();
+	}
+
+	bool isAttacking() {
+		return state == State.ATTACKING;
+	}
+
+	bool isNotAttacking() {
+		return !isAttacking ();
+	}
+
+	void stateRunning() {
+		state = State.RUNNING;
+		animator.SetBool("IsRunning", true);
+		animator.SetBool("IsAttacking", false);
+	}
+
+	void stateAttacking() {
+		Debug.Log ("StateAttacking - start");
+		state = State.ATTACKING;
+		animator.SetBool("IsRunning", false);
+		animator.SetBool("IsAttacking", true);
+		Debug.Log ("StateAttacking - end");
+	}
+
+	void stateIdle() {
+		state = State.IDLE;
+		animator.SetBool("IsRunning", false);
+		animator.SetBool("IsAttacking", false);
+	}
+
+	void locatePosition(out Vector3 position) {
+		position = transform.position;
 		RaycastHit hit;
 		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 
 		if (Physics.Raycast (ray, out hit, RAY_DISTANCE)) {
 			position = hit.point;
 			position.y = 0;
+
+			Quaternion newRotation = Quaternion.LookRotation(position - transform.position, Vector3.forward);
+			newRotation.x = newRotation.z = 0;
+
+			transform.rotation = newRotation;
 		}
 	}
 
 	void moveToPosition() {
 	    getDistanceToPosition();
-	    if (distance > 0.5)
-	    {
-	        animator.SetBool("IsRunning", true);
-	        Quaternion newRotation = Quaternion.LookRotation(position - transform.position, Vector3.forward);
-	        newRotation.x = newRotation.z = 0;
-
-	        transform.rotation = newRotation;
-	        controller.SimpleMove(transform.forward * speed);
-	    }
-	    else
-	    {
-	        animator.SetBool("IsRunning", false);
+		if (distance > 0.5) {
+			controller.SimpleMove(transform.forward * speed);
+	    } else {
+			stateIdle ();
 	    }
 	}
 
     void getDistanceToPosition()
     {
-        distance = Vector3.Distance(transform.position, position);
+        distance = Vector3.Distance(transform.position, movementPosition);
     }
+
+	public void endAttacking() {
+		Debug.Log ("EndAttacking - start");
+		stateIdle ();
+		Debug.Log ("EndAttacking - stop");
+	}
+
+	enum State {
+		IDLE,
+		ATTACKING,
+		RUNNING,
+		DEAD
+	}
 }
